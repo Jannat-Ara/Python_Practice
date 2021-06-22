33e79a57eb2379e16128a538cfa6c7e31897469e
Jannat Ara Ferdouse Raya <59131687+Jannat-Ara@users.noreply.github.com>
2021-06-22 22:59:40 +0600

    second commit for basic

--- basic_01.py
+++ basic_01.py
@@ -1,2 +1,17 @@
 # first code
 print("Hello World")
+
+# type conversion
+birth_year = input("Enter your birth year:")
+age = 2021 - int(birth_year)
+print(age)
+# Exercise_01: Basic calculator
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+sum = float(num1) + int(num2)
+print(sum)
+# another way
+number1 = input("Enter the first number:")
+number2 = input("Enter the second number:")
+sum1 = float(number1)+float(number2)
+print("", str(sum1))
